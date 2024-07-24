70298bf394fc853c5d0c30c58be8f66694a82bf8
Ane Caroline Silveira <68664176+carolinesv-dev@users.noreply.github.com>
2024-07-24 12:14:25 -0300

    biscoito da sorte

--- main.js
+++ main.js
@@ -1,32 +1,56 @@
-const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-const cookieButton = document.querySelector("#cookieButton")
-const resetButton = document.querySelector("#resetButton")
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const cookieButton = document.querySelector("#cookieButton");
+const resetButton = document.querySelector("#resetButton");
 
-document.addEventListener('DOMContentLoaded', function() {
-  cookieButton = document.querySelector('.cookieButton');
+const fortune = [
+  'A vida trará grandes surpresas para você.',
+  'Um novo amor está chegando.',
+  'Boas notícias virão em breve.',
+  'Seja paciente. Há grandes conquistas no seu caminho.',
+  'A sorte está do seu lado hoje.',
+  'Sua criatividade trará sucesso em breve.',
+  'Um sorriso sincero abrirá muitas portas.',
+  'Aproveite cada momento. Eles não voltam mais.',
+  'Confie na sua intuição. Ela nunca falha.',
+  'Um pequeno gesto de gentileza pode gerar um grande impacto.',
+  'O sucesso está na sua perseverança.',
+  'Grandes realizações exigem grandes esforços.',
+  'Acredite nos seus sonhos; eles estão mais próximos do que você imagina.',
+  'A felicidade está nas pequenas coisas da vida.',
+  'A sorte favorece os corajosos.',
+]
 
-  cookieButton.addEventListener('click', function() {
-    // Redireciona para a tela 2
-    toggleScreen()
-  });
-});
+// Eventos
+cookieButton.addEventListener('click', handleClickCookie);
+resetButton.addEventListener('click', handleClickReset);
+document.addEventListener('keydown', eEnter);
 
-document.addEventListener('DOMContentLoaded', function() {
-  cookieButton = document.querySelector('.cookieButton');
-
-  cookieButton.addEventListener('click', function() {
-    // Adiciona a classe para iniciar a animação
-    this.classList.add('tremendo');
-
-    // Remove a classe após a animação terminar
-    setTimeout(() => {
-      cookieButton.classList.remove('tremendo');
-    }, 500)
-  })})
-
-  function toggleScreen() {
-      screen1.classList.toggle('hide');
-      screen2.classList.toggle('hide');
-      
+// Funções
+function eEnter(e) {
+  if (e.key == 'Enter' && screen2.classList.contains('hide')) {
+    handleClickCookie();
+  } else if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleClickReset();
   }
+}
+
+function handleClickCookie() {
+  toggleScreen();
+  pickFortune();
+}
+
+function handleClickReset() {
+  toggleScreen();
+}
+
+function pickFortune() {
+  let fortunes = fortune.length;
+  let randomNumber = Math.floor(Math.random() * fortunes);
+  screen2.querySelector("#fortune").innerText = fortune[randomNumber];
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide');
+  screen2.classList.toggle('hide');
+}
